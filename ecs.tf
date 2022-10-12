4dc7db305b41788d0f8247baf244a113b5b0dc04
Chad Bates <cbates255@gmail.com>
2022-10-12 10:46:01 -0400

    coded

--- ecs.tf
+++ ecs.tf
@@ -2,12 +2,19 @@ resource "docker_image" "centos8" {
   name = var.dockerimage
 }
 
-resource "aws_ecs_cluster" "project_cluster" {
+resource "aws_ecs_cluster" "projectcluster" {
   name = "project_cluster"
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-
 }
+
+resource "aws_ecs_cluster_capacity_providers" "capacity" {
+  cluster_name = aws_ecs_cluster.projectcluster
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
+}
+
